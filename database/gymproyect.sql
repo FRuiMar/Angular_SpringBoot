f79045810182851cb5955bbadf0f8d1266ddf751
Fran Ruiz Martin <franruiz81@gmail.com>
2025-03-03 18:27:45 +0100

    actualizo base datos Angular

--- database/gymproyect.sql
+++ database/gymproyect.sql
@@ -3,22 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2025 a las 01:06:41
+-- Tiempo de generación: 02-03-2025 a las 19:00:03
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
-
-
--- Verifica si la base de datos existe y elimínala si es necesario
-DROP DATABASE IF EXISTS `gymproyect`;
-
--- Crea la base de datos `gymproyect`
-CREATE DATABASE `gymproyect`;
-
--- Usa la base de datos `gymproyect`
-USE `gymproyect`;
-
-
-
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -56,8 +43,8 @@ CREATE TABLE `clases` (
 INSERT INTO `clases` (`id`, `nombre`, `horario`, `capacidad_maxima`, `entrenador_id`, `imagen`) VALUES
 (1, 'Zumba', '2025-02-22 15:26:32', 30, 1, 'asdfasdfasdf'),
 (2, 'Yoga', '2025-02-22 11:29:17', 20, 1, 'asfdjñlkasjñasd'),
-(5, 'Crossfit', '2025-02-24 12:29:17', 20, 1, 'ddd'),
-(6, 'CrestFat232', '2026-02-12 12:43:17', 2, 1, 'AAAAA');
+(5, 'Crossfit', '2025-02-24 12:29:17', 15, 5, 'ddd'),
+(10, 'HIIT', '2025-02-28 22:42:00', 354, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -81,7 +68,8 @@ CREATE TABLE `entrenadores` (
 INSERT INTO `entrenadores` (`id`, `dni`, `nombre`, `apellido`, `especialidad`, `imagen`) VALUES
 (1, '12312312A', 'Pepe1', 'meeeh', 'Zumba y Yoga', 'asfdas'),
 (2, '12341234A', 'Javi', 'Fortote', 'PowerGym y Pilates', 'asfdasfdadsfwer'),
-(3, '33333333', 'probando----ID', 'probandoPor--ID', 'HacerCroquetas', 'prob.ID');
+(3, '33333333', 'probando----ID', 'probandoPor--ID', 'HacerCroquetas', 'prob.ID'),
+(5, '54545454a', 'Fortachon', 'McGee', 'Salsa y Merengue', 'asdfasdfasdfasdaf');
 
 -- --------------------------------------------------------
 
@@ -128,7 +116,10 @@ INSERT INTO `reservas` (`id`, `usuario_id`, `clase_id`, `fecha_reserva`) VALUES
 (1, 1, 1, '2025-02-26 19:50:12'),
 (2, 3, 5, '2025-02-28 19:51:46'),
 (3, 1, 5, '2025-02-28 16:13:55'),
-(5, 3, 1, '2026-02-26 18:50:12');
+(9, 18, 10, '2026-02-26 18:50:12'),
+(10, 18, 5, '2026-02-26 18:50:12'),
+(11, 18, 1, '2026-02-26 18:50:12'),
+(12, 18, 2, '2026-02-26 18:50:12');
 
 -- --------------------------------------------------------
 
@@ -156,7 +147,13 @@ INSERT INTO `usuarios` (`id`, `dni`, `nombre`, `apellido`, `email`, `contrasena`
 (1, '11111111A', 'Fran', 'Ruiz', 'franruiz81@gmail.com', '1234', 'ADMIN', NULL, 1),
 (2, '22222222A', 'Pepe', 'Ruiz', 'pepe@pepe.com', '1234', 'NORMAL', NULL, 1),
 (3, '11111113A', 'Fran', 'Torquemada', 'fran@fran.es', '$2a$10$IK1Ta3tPjahI/iqswnoFL.FQtkabJbj1ooeqzBNMiXgWQ8l1zyiA2', 'ADMIN', NULL, 3),
-(7, '11111199Z', 'eeeesssaasfasdf', 'eeeeasdfasee', 'c@sdsdfsdfsc.es', '$2a$10$yVS6qftP32qG3h2/Pyg8kuoLUl3wvS.C3AquOYXaRU5CMLkeXhVDa', 'NORMAL', NULL, 3);
+(7, '11111199Z', 'eeeesssaasfasdf', 'eeeeasdfasee', 'c@sdsdfsdfsc.es', '$2a$10$yVS6qftP32qG3h2/Pyg8kuoLUl3wvS.C3AquOYXaRU5CMLkeXhVDa', 'NORMAL', NULL, 3),
+(11, '12341234a', 'fras', 'fras', 'f@f.es', '$2a$10$Yt7a4kogWN9KTqeJgAJuiOPavP92ww40E0aIHlLj/nN6UYZ2PGxqu', 'NORMAL', '', 1),
+(14, '1194994A', 'Aaaaaaaaaa', 'Naaaaaaaaaaa', 'Coaaaaa@conaaaaaaa.es', '$2a$10$DsIaY97T9SNIWwbQFUEbU.lpZ.l7uzmDNneAKk96M.V2iRvDHLSAC', 'NORMAL', NULL, 5),
+(15, '12344321a', 'Fran', 'Ruiz', 'f@f.com', '$2a$10$.k6VJsRFWqwLrj3W3oSEpe9NkKp9MS0npp4d9lSS36clZHweLjps6', 'NORMAL', '', 1),
+(18, '2292344A', 'frrraaaan', 'Nabnosor32aaaffffff', 'f2@f.com', '$2a$10$paRU31joYUU3bnXFghr4jeCCgDFhzSRQU0SZpJnb6qYZpaXnYlwdK', 'ADMIN', NULL, 1),
+(19, '43211234A', 'Julito', 'Iglesias', 'j@j.com', '$2a$10$po85KyEnoQFyBJ0ZERK.iuVfoQBoIMD4uLm3Gc07PzcsQnOz7Bvme', 'NORMAL', '', 2),
+(20, '43144332a', 'bbddddddddddddddddddddddbb', 'bbbbb111111111bbbbb', 'a@a.es', '$2a$10$xapHg24AsZy88Xf1B6aBmOO.62h1zJk5B6O8sek9ULHITcRfNVv6W', 'NORMAL', NULL, 1);
 
 --
 -- Índices para tablas volcadas
@@ -207,13 +204,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `clases`
 --
 ALTER TABLE `clases`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `entrenadores`
 --
 ALTER TABLE `entrenadores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `membresias`
@@ -225,13 +222,13 @@ ALTER TABLE `membresias`
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restricciones para tablas volcadas
